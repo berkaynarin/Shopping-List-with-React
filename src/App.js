544f3d39cb6112be53c2266bd203f1aeae64b064
Berkay Narin <bnarin.dev@gmail.com>
2021-09-07 16:16:15 +0300

    Mapped an object with list items

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["item1", "item2", "item3", "item4"]
   };
 
   render() {
-    let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <div className="App">
-        <span className={classes}>{this.handleZero()}</span>
+        <span className={this.getBadgeClasses()}>{this.handleZero()}</span>
         <button className="btn btn-primary">Increment</button>
+        <ul>
+          {this.state.tags.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge text-dark m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   handleZero() {
