ebe5025ae6d192c5c64e8a5167730700bfd3d8df
Berkay Narin <bnarin.dev@gmail.com>
2021-09-16 14:39:13 +0300

    Add a function to increment the badge with button

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ class App extends Component {
     tags: ["item1", "item2", "item3", "item4"]
   };
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+    //Increments the state
+  };
+
   render() {
     return (
       <div className="App">
         <span className={this.getBadgeClasses()}>{this.handleZero()}</span>
-        <button className="btn btn-primary">Increment</button>
+        <button onClick={this.handleIncrement} className="btn btn-primary">
+          Increment
+        </button>
         <ul>
           {this.state.tags.map((item) => (
             <li key={item}>{item}</li>
@@ -25,6 +32,7 @@ class App extends Component {
   getBadgeClasses() {
     let classes = "badge text-dark m-2 bg-";
     classes += this.state.count === 0 ? "warning" : "primary";
+    //check the count and then add a bootstrap class to change the badge
     return classes;
   }
 
